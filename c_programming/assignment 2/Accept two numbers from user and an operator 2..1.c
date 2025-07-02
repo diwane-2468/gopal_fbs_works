3430f88c358787de579288c436ff524957a69ad7
Gopal Diwane <diwanegopal0@gmail.com>
2025-07-02 19:59:11 +0530

    Add files via upload

---
+++ c_programming/assignment 2/Accept two numbers from user and an operator 2..1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Input operator
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &op); 
+    
+        case '+':
+            printf("Result = %d\n", num1 + num2);
+            brea
