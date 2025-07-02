313e40b58e8f04e30fd8ded6f980d09a6e7a61db
Gopal Diwane <diwanegopal0@gmail.com>
2025-07-02 19:29:58 +0530

    Add files via upload

---
+++ c_programming/assignment 0/square and cube of a given number..cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number = 5;  
+    int square, cube;
+
+    square = number * number;
+    cube = number * number * number;
+
+    printf("Number: %d\n", number);
+    printf("Square: %d\n", square);
+    printf("Cube: %d\n", cube);
+
+    return 0;
+}
