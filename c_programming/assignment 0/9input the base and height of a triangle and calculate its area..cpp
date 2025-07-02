f6e91c5073c0dfc45a4954dba641fd7f5e865d89
Gopal Diwane <diwanegopal0@gmail.com>
2025-07-02 19:33:46 +0530

    Add files via upload

---
+++ c_programming/assignment 0/9input the base and height of a triangle and calculate its area..cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float base = 10.0;
+    float height = 5.0;
+    float area;
+
+    area = 0.5 * base * height;
+
+    
+    printf("Base = %.2f, Height = %.2f\n", base, height);
+    printf("Area of the triangle = %.2f\n", area);
+
+    return 0;
+}
